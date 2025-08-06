70228396e0830eb10168e11cc51157117fb20b57
lbaron2 <lbaron@tda.com>
2025-08-06 14:17:16 -0600

    Added a way to read private info without GitHub leaks

--- mainPage.py
+++ mainPage.py
@@ -1,6 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+def readSens() -> None:
+    #reads in values that are not supposed to be on github through a file called "sensitive.txt", such as website access passwords or API keys
+    import os
+    global sensitive
 
-driver = webdriver.Chrome(keep_alive=True)
+    sensitive = {}
+    if "sensitive.txt" in os.listdir():
+        with open(r"sensitive.txt", "r") as txt:
+            for line in txt:
+                words = line.strip().split(":")
+                sensitive[words[0].upper()] = words[1]
+                
+def mainWebsiteTest():
+    pass
 
-driver = driver.get("https://tda.com/")
+if __name__ == "__main__":
+    readSens()
+    mainWebsiteTest()
