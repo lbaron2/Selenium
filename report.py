e63a558afcfd5940fdb422c513cf3780ce559538
lbaron2 <lbaron@tda.com>
2026-03-19 06:41:48 -0600

    Changed report to be function based

--- report.py
+++ report.py
@@ -2,13 +2,10 @@ import tests
 import setup
 import datetime
 
-if __name__ == "__main__":
-    
-    setup.readSens()
+def run():
     driver = setup.setup()
     pages = setup.findAllPages(driver)
-    
-    with open(f"report.md","w+") as output: 
+    with open(f"reports/report_{datetime.datetime.today().strftime("%Y-%m-%d")}.md","w+") as output: 
 
         errorCount = 0
         pageError = 0
@@ -17,9 +14,9 @@ if __name__ == "__main__":
             driver.get(page)
             priorErrorCount = errorCount
 
-            errorCount += setup.ifError(tests.ifConnects(driver),page,"Connection",output)
-            errorCount += setup.ifError(tests.if404(driver),page,"404",output)
-            errorCount += setup.ifError(tests.ifMetaDesc(driver),page,"Meta Description",output)
+            # errorCount += setup.ifError(tests.ifConnects(driver),page,"Connection",output)
+            # errorCount += setup.ifError(tests.if404(driver),page,"404",output)
+            # errorCount += setup.ifError(tests.ifMetaDesc(driver),page,"Meta Description",output)
 
             imgs = tests.findImgs(driver)
 
@@ -29,10 +26,9 @@ if __name__ == "__main__":
             if(errorCount != priorErrorCount):
                 pageError += 1
                 print(file=output)
-            
-
 
         print(f"Out of {len(pages)}, there were {pageError} page(s) with errors a total number of {errorCount} errors across all pages", file=output)
-
+        print(f"The seen pages were: {pages}", file=output)
+        print(f"This report was done on: {datetime.datetime.today().strftime("%Y-%m-%d")}", file=output)
 
     setup.close(driver)
